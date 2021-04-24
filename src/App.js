ebcb473de8a194de2bb2ad5cce7cea5150d3349f
DevGenix <you@example.com>
2021-04-24 13:28:08 -0400

    Linked to Footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Auth from './Components/Auth/Auth'
+import Footer from "./Components/Footer/Footer"
 
 function App() {
     return (
         <div>
             <Auth />
+            <Footer />
         </div>
     )
 }
