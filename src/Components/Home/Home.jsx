8d8bd01b3ccf76f37c0b061d17619a7480e44de2
DevGenix <you@example.com>
2021-04-27 07:56:18 -0400

    Dashboard icons

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,6 +4,7 @@ import "./Home.css"
 import Todo from "../Todo/Todo"
 import {useAuthState} from "react-firebase-hooks/auth"
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import DashboardIcon from '@material-ui/icons/Dashboard';
 
 
 function Home() {
@@ -16,6 +17,7 @@ function Home() {
                     <h3>TasksKnockout</h3>
                 </div>
                 <div className="userAccountOptions">
+                    <DashboardIcon className="siteUpdatesIcons"/>
                     <NotificationsIcon className="siteUpdatesIcons"/>
                     <button className="signOut" onClick={() => auth.signOut()}>Sign Out</button>
                     <img className="userProfile" src={user.photoURL} />
