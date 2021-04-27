490b3fdcba7ca08aafbfae307b226bed59fa5748
DevGenix <you@example.com>
2021-04-26 22:21:06 -0400

    Fix typo

--- src/Components/Todo/Todo.jsx
+++ src/Components/Todo/Todo.jsx
@@ -6,7 +6,7 @@ function Todo() {
         <div className="todoBoxContainer">
             <div className="todoBoxInner">
                 <div className="todoInputsContainer">
-                    <form className="todoFormContainer" action="sumbmit">
+                    <form className="todoFormContainer" action="submit">
                         <div className="todoAddContainer">
                             <AddIcon className="addTodoIcon"/>
                             <input placeholder="   What are your tasks?" className="todoFormInput"/>
