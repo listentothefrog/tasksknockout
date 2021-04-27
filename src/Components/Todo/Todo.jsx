a9808b21a293ba0860ae239f3168c3d036df53b2
DevGenix <you@example.com>
2021-04-26 20:58:53 -0400

    Todo Prototype  UI

---
+++ src/Components/Todo/Todo.jsx
@@ -0,0 +1,24 @@
+import "./Todo.css"
+import AddIcon from '@material-ui/icons/Add';
+
+function Todo() {
+    return (
+        <div className="todoBoxContainer">
+            <div className="todoBoxInner">
+                <div className="todoInputsContainer">
+                    <form className="todoFormContainer" action="sumbmit">
+                        <div className="todoAddContainer">
+                            <AddIcon className="addTodoIcon"/>
+                            <input placeholder="   What are your tasks?" className="todoFormInput"/>
+                        </div>
+                    </form>
+                    <div className="userTodosContainer">
+                        <h1>Your Tasks</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Todo
